2e319345d7d3400b44edd47492eaefc8d186c2c8
Ejike <ejike.chiboka@gmail.com>
2019-05-16 12:04:10 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,30 +4,34 @@ var cards = ["queen", "queen", "king", "king"]
 
 var cardsInPlay = []
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-cardsInPlay;
-
-console.log("user flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-cardsInPlay;
-console.log("user flipped " + cardTwo);
-
-var cardsInPlay = ["queen", "king"];
-
-if (cardsInPlay.length === 2) 
+function checkForMatch()
 {
   if (cardsInPlay[0] === cardsInPlay[1]) 
   {
-    alert("You found a match!");	
+    console.log("You found a match!");	
   } 
   else 
   {
-	alert("Sorry, try again.");
+	console.log("Sorry, try again.");
   }
 
+};
+
+function flipCard(cardId)
+{
+if (cardsInPlay.length === 2) 
+{
+
+
 } 
+cardsInPlay.push(cards[cardId]);
+
+console.log("user flipped " + cards[cardId]);
+};
+flipCard(0);
+flipCard(2);
+checkForMatch();
+
+
 
 
